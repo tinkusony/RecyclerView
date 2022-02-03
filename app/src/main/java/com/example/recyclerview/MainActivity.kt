e924f72ba518e287cb05b080fa10187955b8c3ec
tinkusony <tinkusony90@gmail.com>
2022-02-03 23:47:15 +0530

    add button click event and hide virtual keyboard.

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.recyclerview
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        hideKeyboard()
+        val edit = editText.text.toString()
+        val i = Intent(this, SecondActivity::class.java)
+            .apply {
+                putExtra("key", edit)
+            }
+        startActivity(i)
+    }
+
+    private fun hideKeyboard() {
+        val view = this.currentFocus
+        if (view != null) {
+            val hide = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            hide.hideSoftInputFromWindow(view.windowToken, 0)
+        } else {
+            window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+        }
     }
 }
--- app/src/main/java/com/example/recyclerview/SecondActivity.kt
+++ app/src/main/java/com/example/recyclerview/SecondActivity.kt
@@ -2,10 +2,14 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val msg = intent.getStringExtra("key")
+        message.text = msg
     }
 }
